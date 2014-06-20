98311ec0c028caa1297fc3bfe3d387f0524fcc14
John Bartko <jbartko@txstate.edu>
2014-06-19 21:54:13 -0500

    Parameterize home permissions
    Allow home permissions to be overridden in Hiera data.

--- manifests/user.pp
+++ manifests/user.pp
@@ -12,6 +12,7 @@ define accounts::user(
   $username = $title,
   $managehome = true,
   $home = undef,
+  $home_permissions = '0755',
   $ensure = present,
   $recurse_permissions = false,
 ) {
@@ -80,7 +81,7 @@ define accounts::user(
           owner   => $username,
           group   => $username,
           recurse => $recurse_permissions,
-          mode    => '0755',
+          mode    => $home_permissions,
         }
 
         file { "${home_dir}/.ssh":
