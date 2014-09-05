d9347210a974b948212eeb3d2f0c6fb7a9b66f0f
John Bartko <jbartko@txstate.edu>
2014-09-04 19:26:39 -0500

    Allow for the existence of groups to be controlled via an ensure parameter.

--- manifests/group.pp
+++ manifests/group.pp
@@ -1,8 +1,16 @@
 # Definition of a Linux/Unix group
 #
-define accounts::group ($gid, $groupname=$title) {
+define accounts::group (
+  $groupname    = $title,
+  $group_ensure = 'present',
+  $gid          = undef,
+) {
+
+  validate_re($group_ensure, [ '^absent$', '^present$' ],
+    'The $group_ensure parameter must be \'absent\' or \'present\'')
+
   group { $groupname:
-    ensure  => present,
-    gid     => $gid
+    ensure => $group_ensure,
+    gid    => $gid,
   }
 }
