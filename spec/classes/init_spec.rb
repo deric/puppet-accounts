5529f1ceeab3a5699108f950059d326f1c1ee67d
Tomas Barton <barton.tomas@gmail.com>
2015-01-29 10:18:07 +0100

    specs for #9

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,4 +11,21 @@ describe 'accounts' do
   it { should contain_class('accounts::users') }
   it { should contain_class('accounts::groups') }
 
+  context 'allow passing users and groups directly to init class' do
+    let(:params){{
+      :users => { 'john' => { 'comment' => 'John Doe', 'gid' => 2001 }},
+      :groups => { 'developers' => { 'gid' => 2001 }}
+    }}
+
+    it { should contain_user('john').with(
+      'comment' => 'John Doe',
+      'gid' => 2001
+    )}
+
+    it { should contain_group('developers').with(
+      'gid'    => 2001,
+      'ensure' => 'present'
+    )}
+
+  end
 end
