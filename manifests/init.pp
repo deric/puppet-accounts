04fb7a7aaf6c3a15d57837f82fec951bde444f29
Tomas Barton <barton.tomas@gmail.com>
2015-01-29 10:22:52 +0100

    merge hiera hash with puppet hash, should close #9

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,29 @@
 # Puppet accounts management
 #
 class accounts(
-  $manage_users       = hiera('accounts::manage_users', true),
-  $manage_groups      = hiera('accounts::manage_groups', true),
+  $manage_users  = hiera('accounts::manage_users', true),
+  $manage_groups = hiera('accounts::manage_groups', true),
+  $users         = {},
+  $groups        = {},
 ) {
-  $users              = hiera_hash('accounts::users', {})
-  $groups             = hiera_hash('accounts::groups', {})
+  validate_bool($manage_users)
+  validate_bool($manage_groups)
+  validate_hash($users)
+  validate_hash($groups)
+
+  $users_h  = hiera_hash('accounts::users', {})
+  $groups_h = hiera_hash('accounts::groups', {})
+
+  $merged_users = merge($users, $users_h)
+  $merged_groups = merge($groups, $groups_h)
 
   class { 'accounts::groups':
     manage => $manage_groups,
-    groups => $groups,
+    groups => $merged_groups,
   }
 
   class { 'accounts::users':
     manage => $manage_users,
-    users  => $users,
+    users  => $merged_users,
   }
 }
