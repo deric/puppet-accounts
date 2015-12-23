891d99820da61f2c9f72798db1ca50dae6f8e22b
Simon Beirnaert <simon.beirnaert@gmail.com>
2015-12-23 15:24:11 +0100

    Allow default values for accounts::user to be passed along.

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,13 +1,15 @@
 # Manages users accounts if enabled
 #
 class accounts::users (
-  $users  = {},
-  $manage = true,
+  $users    = {},
+  $manage   = true,
+  $defaults = {},
 ) {
   validate_bool($manage)
   validate_hash($users)
+  validate_hash($defaults)
 
   if $manage {
-    create_resources(accounts::user, $users)
+    create_resources(accounts::user, $users, $defaults)
   }
 }
