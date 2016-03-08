27165fb73cdf53d408819994fb9934b746e5af4d
Tomas Barton <barton.tomas@gmail.com>
2016-03-09 00:17:55 +0100

    change ordering of creating resources #38

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,16 +19,17 @@ class accounts(
   $merged_users = merge($users, $users_h)
   $merged_groups = merge($groups, $groups_h)
 
+  # first create users, then assign users to the groups
   class { 'accounts::groups':
     manage => $manage_groups,
     users  => $merged_users,
     groups => $merged_groups,
+    require  => Class['accounts::users']
   }
 
   class { 'accounts::users':
     manage   => $manage_users,
     users    => $merged_users,
     defaults => $user_defaults,
-    require  => Class['accounts::groups']
   }
 }
