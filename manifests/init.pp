4a793316964f6b63e17c25dfdb4868ca14f33baf
Tomas Barton <barton.tomas@gmail.com>
2016-03-15 00:37:32 +0100

    remove resource ordering (should be handled individually)

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,19 +19,14 @@ class accounts(
   $merged_users = merge($users, $users_h)
   $merged_groups = merge($groups, $groups_h)
 
-  anchor { 'accounts::primary_groups_created': }
-  # create primary groups first
   $primary_groups = accounts_primary_groups($merged_users)
   notify{"primary_groups: ${primary_groups}": }
-  create_resources(accounts::group, $primary_groups,
-    {'before' => Anchor['accounts::primary_groups_created']}
-  )
+  create_resources(accounts::group, $primary_groups)
 
   class { 'accounts::users':
     manage   => $manage_users,
     users    => $merged_users,
     defaults => $user_defaults,
-    require  => Anchor['accounts::primary_groups_created'],
   }
 
   # first create users, then assign users to the groups
@@ -39,6 +34,5 @@ class accounts(
     manage  => $manage_groups,
     users   => $merged_users,
     groups  => $merged_groups,
-    require => Class['accounts::users']
   }
 }
