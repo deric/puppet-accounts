941172713c994c77f17ec0a5d07556090f711b2a
Tomas Barton <barton.tomas@gmail.com>
2016-07-08 13:47:35 +0200

    ensure that ssh_authorized_key resource is created on puppet 3.4 #47
     * rewrite authorized_keys file after puppet resource is created

--- manifests/authorized_keys.pp
+++ manifests/authorized_keys.pp
@@ -33,6 +33,26 @@ define accounts::authorized_keys(
     'type'  => 'ssh-rsa',
   }
 
+  anchor { "accounts::auth_keys_created_${title}": }
+
+  # backwards compatibility only - will be removed in 2.0
+  # see https://github.com/deric/puppet-accounts/issues/40
+  if !empty($ssh_key) {
+    ssh_authorized_key { "${username}_${ssh_key['type']}":
+      ensure  => present,
+      user    => $username,
+      type    => $ssh_key['type'],
+      key     => $ssh_key['key'],
+      options => $ssh_key['options'],
+      require =>  File[$auth_keys],
+      before  => Anchor["accounts::auth_keys_created_${title}"],
+    }
+  }
+
+  if !empty($ssh_keys) {
+    create_resources('ssh_authorized_key', $ssh_keys, $ssh_key_defaults)
+  }
+
   # prior to Puppet 3.6 `purge_ssh_keys` is not supported
   if versioncmp($::puppetversion, '3.6.0') < 0 and $purge_ssh_keys {
     if !empty($ssh_keys) or !empty($ssh_key) {
@@ -42,27 +62,11 @@ define accounts::authorized_keys(
         group   => $real_gid,
         mode    => '0600',
         content => template("${module_name}/authorized_keys.erb"),
-        require => File["${home_dir}/.ssh"],
+        require => [File["${home_dir}/.ssh"], Anchor["accounts::auth_keys_created_${title}"]],
       }
+      Ssh_authorized_key<| |> -> File[$auth_keys]
     }
   } else {
-    # backwards compatibility only - will be removed in 2.0
-    # see https://github.com/deric/puppet-accounts/issues/40
-    if !empty($ssh_key) {
-      ssh_authorized_key { "${username}_${ssh_key['type']}":
-        ensure  => present,
-        user    => $username,
-        type    => $ssh_key['type'],
-        key     => $ssh_key['key'],
-        options => $ssh_key['options'],
-        require =>  File[$auth_keys],
-      }
-    }
-
-    if !empty($ssh_keys) {
-      create_resources('ssh_authorized_key', $ssh_keys, $ssh_key_defaults)
-    }
-
     file { $auth_keys:
       ensure  => $ensure,
       owner   => $username,
