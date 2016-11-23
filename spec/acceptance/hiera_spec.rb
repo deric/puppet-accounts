7db735564bda620d10f5e712673b097001d3fdd9
Tomas Barton <barton.tomas@gmail.com>
2016-11-23 14:55:36 +0100

    acceptance tests for #39

--- spec/acceptance/hiera_spec.rb
+++ spec/acceptance/hiera_spec.rb
@@ -62,5 +62,77 @@ EOS
       its(:stdout) { is_expected.to match /test/ }
     end
   end
+
+  context 'avoid cyclic dependency' do
+    let(:yaml) do
+<<-EOS
+classes:
+  - '::accounts'
+accounts:
+ user_defaults:
+   authorized_keys_file: '/etc/ssh/%u/authorized_keys'
+   manage_group: true
+   shell: "/bin/bash"
+accounts::users:
+  it:
+    comment: "IT Admin"
+    managehome: true
+    pwhash: '$1$dn3lUNjy$i00w1UuAzN7M/yNtGX0a9/'
+    groups: ['sudo', 'it']
+  jordi:
+    comment: "Jordi Garcia"
+    groups: ['sudo', 'it']
+    shell: '/bin/bash'
+EOS
+    end
+
+        let(:pp) do
+<<-EOS
+hiera_include('classes')
+EOS
+    end
+
+    it 'install accounts' do
+      shell "echo \"#{yaml}\" > #{HIERA_PATH}/hieradata/common.yaml"
+
+      expect(apply_manifest(pp,
+        :catch_failures => false,
+        :debug => false
+      ).exit_code).to be_zero
+    end
+
+    describe group('it') do
+      it { is_expected.to exist }
+    end
+
+    describe user('jordi') do
+      it { is_expected.to exist }
+    end
+
+    describe command('getent group it') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) { is_expected.to match /it:x:(\d+):it/ }
+    end
+
+    describe file('/home/jordi') do
+      it { is_expected.to be_directory }
+    end
+
+    describe command('cat /etc/passwd | grep it') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) { is_expected.to match /it/ }
+    end
+
+    describe command('groups jordi') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) { is_expected.to match /jordi : jordi sudo/ }
+    end
+
+    describe command('groups it') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) { is_expected.to match /it : it sudo/ }
+    end
+
+  end
 end
 
