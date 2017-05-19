41d67ce1a5811795b298b66ea4517b8563adfd44
Tomas Barton <barton.tomas@gmail.com>
2017-05-19 14:34:38 +0200

    replace gpasswd by usermod

---
+++ lib/puppet/provider/group/usermod.rb
@@ -0,0 +1,118 @@
+require 'puppet/provider/group/groupadd'
+
+Puppet::Type.type(:group).provide :usermod, :parent => Puppet::Type::Group::ProviderGroupadd do
+  require 'shellwords'
+
+  desc <<-EOM
+    Group management via `usermod`. This allows for local group
+    management when the users exist in a remote system.
+  EOM
+
+  commands  :addmember => 'usermod',
+            :delmember => 'usermod',
+            :modmember => 'usermod'
+
+  has_feature :manages_members unless %w{HP-UX}.include? Facter.value(:operatingsystem)
+  has_feature :libuser if Puppet.features.libuser?
+
+  def addcmd
+    # This pulls in the main group add command should the group need
+    # to be added from scratch.
+    cmd = Array(super.map{|x| x = "#{x}"}.shelljoin)
+
+    @resource[:members] and cmd += @resource[:members].map do |x|
+      [ command(:addmember),'-a',x,@resource[:name] ].shelljoin
+    end
+
+    # A bit hacky way how to update /etc/group in a single shell session
+    # Executing [ 'groupadd somegrp', 'gpasswd -a user somegrp'] does not modify
+    # /etc/group in current session (two Puppet runs are required).
+    # see: https://github.com/deric/puppet-accounts/issues/60
+    if @resource[:members] and @resource[:members].size == 1
+      user = @resource[:members].first
+      cmd << "sed -i.bak -e 's/^\\(#{user}\\)\\(.*\\)/\\1\\2#{user}/g' /etc/group"
+    end
+
+    mod_group(cmd)
+
+    # We're returning /bin/true here since the Nameservice classes
+    # would execute whatever is returned here.
+    return '/bin/true'
+  end
+
+  # This is a repeat from puppet/provider/nameservice/objectadd.
+  # The self.class.name matches are hard coded so cannot be easily
+  # overridden.
+  def modifycmd(param, value)
+    cmd = [command(param.to_s =~ /password_.+_age/ ? :password : :modify)]
+    cmd << flag(param) << value
+    if @resource.allowdupe? and (param == :gid)
+      cmd << "-o"
+    end
+    cmd << @resource[:name]
+
+    cmd
+  end
+
+  def members
+    getinfo(true) if @objectinfo.nil?
+    retval = @objectinfo.mem
+
+    if ( @resource[:attribute_membership] == :minimum ) and !retval.nil? and
+       !@resource[:members].nil? and (@resource[:members] - retval).empty?
+    then
+      retval = @resource[:members]
+    else
+      retval = @objectinfo.mem
+    end
+
+    retval.sort
+  end
+
+  def members=(members)
+    cmd = []
+    to_be_added = members.dup.sort!
+    if @resource[:attribute_membership] == :minimum
+      to_be_added = to_be_added | @objectinfo.mem
+      not to_be_added.empty? and cmd += to_be_added.map { |x|
+        [ command(:addmember),'-a',x,@resource[:name] ].shelljoin
+      }
+      mod_group(cmd)
+    else
+      # inclusive strategy
+      # assuming that provided members are complete set
+      unless to_be_added.empty?
+        cmd << [ command(:modmember),'-M',to_be_added.join(','), @resource[:name] ].shelljoin
+        mod_group(cmd)
+      end
+    end
+   end
+
+
+  private
+
+  # This define takes an array of commands to run and executes them in
+  # order to modify the group memberships on the system.
+  # A useful warning message is output if there is an issue modifying
+  # the group but all members that can be added are added. This is an
+  # attempt to do the "right thing" without actually breaking a run
+  # or creating a whole new type just to override an insignificant
+  # segment of the native group type.
+  #
+  # The run of the type *will* succeed in this case but fail in all
+  # others.
+  def mod_group(cmds)
+    cmds.each do |run_cmd|
+      begin
+        execute(run_cmd,{:custom_environment => @custom_environment, :failonfail => true, :combine => true})
+      rescue Puppet::ExecutionFailure => e
+        if $?.exitstatus == 3 then
+          Puppet.warning("Modifying #{@resource[:name]} => #{e}")
+        else
+          raise e
+        end
+      end
+      Puppet.debug("Success: #{run_cmd}")
+    end
+  end
+end
---
+++ spec/unit/provider/group/gpasswd_spec.rb
@@ -0,0 +1,157 @@
+#!/usr/bin/env rspec
+require 'spec_helper'
+
+describe Puppet::Type.type(:group).provider(:usermod) do
+  before do
+    described_class.stubs(:command).with(:add).returns '/usr/sbin/groupadd'
+    described_class.stubs(:command).with(:delete).returns '/usr/sbin/groupdel'
+    described_class.stubs(:command).with(:modify).returns '/usr/sbin/groupmod'
+    described_class.stubs(:command).with(:addmember).returns '/usr/sbin/usermod'
+    described_class.stubs(:command).with(:delmember).returns '/usr/sbin/usermod'
+    described_class.stubs(:command).with(:modmember).returns '/usr/sbin/usermod'
+  end
+
+  let(:resource) { Puppet::Type.type(:group).new(:name => 'mygroup', :provider => provider) }
+  let(:provider) { described_class.new(:name => 'mygroup') }
+
+  describe "#create" do
+    it "should add -o when allowdupe is enabled and the group is being created" do
+      resource[:allowdupe] = :true
+      resource[:gid] = '555'
+      # This is an unfortunate hack to prevent the parent class from
+      # breaking when we execute everything in gpasswd instead of
+      # returning the expected string to execute.
+      provider.expects(:execute).with('/bin/true',
+        :custom_environment => {},
+        :failonfail => true,
+        :combine => true)
+      provider.expects(:execute).with('/usr/sbin/groupadd -g 555 -o mygroup',
+        :custom_environment => {},
+        :failonfail => true,
+        :combine => true
+      )
+      provider.create
+    end
+
+    describe "on system that feature system_groups", :if => described_class.system_groups? do
+      it "should add -r when system is enabled and the group is being created" do
+        resource[:system] = :true
+        provider.expects(:execute).with('/usr/sbin/groupadd -r mygroup',
+          :custom_environment => {},
+          :failonfail => true,
+          :combine => true
+        )
+        provider.expects(:execute).with('/bin/true',
+          :custom_environment => {},
+          :failonfail => true,
+          :combine => true)
+        provider.create
+      end
+    end
+
+    describe "on system that do not feature system_groups", :unless => described_class.system_groups? do
+      it "should not add -r when system is enabled and the group is being created" do
+        resource[:system] = :true
+        provider.expects(:execute).with('/bin/true',
+          :custom_environment => {},
+          :failonfail => true,
+          :combine => true)
+        provider.expects(:execute).with('/usr/sbin/groupadd mygroup',
+          :custom_environment => {},
+          :failonfail => true,
+          :combine => true
+        )
+        provider.create
+      end
+    end
+
+    describe "when adding additional group members to a new group" do
+      it "should pass all members individually as group add options to gpasswd" do
+        provider.expects(:execute).with('/bin/true',
+          :custom_environment => {},
+          :failonfail => true,
+          :combine => true)
+        provider.expects(:execute).with('/usr/sbin/groupadd mygroup',
+          :custom_environment => {},
+          :failonfail => true,
+          :combine => true)
+        resource[:members] = ['test_one','test_two','test_three']
+        resource[:members].each do |member|
+          provider.expects(:execute).with("/usr/sbin/usermod -a -G mygroup #{member}",
+          :custom_environment => {},
+          :failonfail => true,
+          :combine => true
+          )
+        end
+        provider.create
+      end
+    end
+
+    describe "when adding additional group members to an existing group with no members" do
+      it "should add all new members" do
+        Etc.stubs(:getgrnam).with('mygroup').returns(
+          Struct::Group.new('mygroup','x','99999',[])
+        )
+        resource[:attribute_membership] = 'minimum'
+        resource[:members] = ['test_one','test_two','test_three']
+        resource[:members].each do |member|
+          provider.expects(:execute).with("/usr/sbin/usermod -a -G mygroup #{member}",
+            {
+              :custom_environment => {},
+              :failonfail => true,
+              :combine => true
+            }
+          )
+        end
+        provider.create
+        provider.members=(resource[:members])
+      end
+    end
+
+    describe "when adding additional group members to an existing group with members" do
+      it "should add all new members and preserve all existing members" do
+        old_members = ['old_one','old_two','old_three','test_three']
+        Etc.stubs(:getgrnam).with('mygroup').returns(
+          Struct::Group.new('mygroup','x','99999',old_members)
+        )
+        resource[:attribute_membership] = 'minimum'
+        resource[:members] = ['test_one','test_two','test_three']
+        (resource[:members] | old_members).each do |member|
+          provider.expects(:execute).with("/usr/sbin/usermod -a -G mygroup #{member}",
+            {:custom_environment => {},
+          :failonfail => true,
+          :combine => true})
+        end
+        provider.create
+        provider.members=(resource[:members])
+      end
+    end
+
+    describe "when adding exclusive group members to an existing group with members" do
+      it "should add all new members and delete all, non-matching, existing members" do
+        old_members = ['old_one','old_two','old_three','test_three']
+        members = ['queens','darwin','kings','trinity']
+        Etc.stubs(:getgrnam).with('mygroup').returns(
+          Struct::Group.new('mygroup','x','1234',old_members)
+        )
+        resource[:attribute_membership] = :inclusive
+        resource[:members] = members
+        provider.expects(:execute).with("/usr/sbin/usermod -M #{members.sort.join(',')} mygroup",
+            {:custom_environment => {},
+          :failonfail => true,
+          :combine => true})
+        provider.create
+        provider.members=(members)
+      end
+    end
+  end
+
+  describe "#gid=" do
+    it "should add -o when allowdupe is enabled and the gid is being modified" do
+      resource[:allowdupe] = :true
+      provider.expects(:execute).with(['/usr/sbin/groupmod', '-g', 150, '-o', 'mygroup'])
+      provider.gid = 150
+    end
+  end
+end
+
