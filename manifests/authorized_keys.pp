fb4930e4b0c41264d404599c9fed86bbd39fbf54
Tomas Barton <barton.tomas@gmail.com>
2017-06-02 14:34:24 +0200

    removed authorized keys statement - backward compatibility (#40)
      - it caused creating authorized_keys file with incorrect permissions

--- manifests/authorized_keys.pp
+++ manifests/authorized_keys.pp
@@ -66,21 +66,6 @@ define accounts::authorized_keys(
     require => Anchor["accounts::ssh_dir_created_${title}"],
   }
 
-  # backwards compatibility only - will be removed in 2.0
-  # see https://github.com/deric/puppet-accounts/issues/40
-  if !empty($ssh_key) {
-    ssh_authorized_key { "${username}_${ssh_key['type']}":
-      ensure  => present,
-      user    => $username,
-      type    => $ssh_key['type'],
-      key     => $ssh_key['key'],
-      options => $ssh_key['options'],
-      target  => $auth_key_file,
-      before  => Anchor["accounts::auth_keys_created_${title}"],
-      require => Anchor["accounts::ssh_dir_created_${title}"],
-    }
-  }
-
   # prior to Puppet 3.6 `purge_ssh_keys` is not supported
   # `purge_ssh_keys` is defined on User resource
   if versioncmp($::puppetversion, '3.6.0') < 0 {
